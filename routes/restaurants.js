50ef26cf46bef2fc42eafee24c08bc145e2f97a4
KotNatthanan <155931940+KotNatthanan@users.noreply.github.com>
2024-03-02 20:48:30 +0700

    update

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital, getVacCenters} = require('../controllers/restaurant');
+const express = require("express");
+const {
+  getRestaurants,
+  getRestaurant,
+  createRestaurant,
+  updateRestaurant,
+  deleteRestaurant
+} = require("../controllers/restaurant");
 
 
-const appointmentRouter = require('./reservations');
-
-const {protect, authorize} = require('../middleware/auth');
 const router = express.Router();
 
+const { protect, authorize } = require("../middleware/auth");
 
-const { UUID } = require('mongodb');
-const Hospital = require('../models/Restaurant');
 
-router.use('/:hospitalId/appointments/',appointmentRouter);
-router.route('/vacCenters').get(getVacCenters);
 
-router.route('/').get(getHospitals).post(protect, authorize('admin'),  createHospital);
-router.route('/:id').get(getHospital).put(protect, authorize('admin'),updateHospital).delete(protect, authorize('admin'),deleteHospital);
+router
+  .route("/")
+  .get(getRestaurants) 
+  .post(protect, authorize("admin"), createRestaurant);
+router
+  .route("/:id")
+  .get(getRestaurant)
+  .put(protect, authorize("admin"), updateRestaurant)
+  .delete(protect, authorize("admin"), deleteRestaurant);
 
 module.exports = router;
-
