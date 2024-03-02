5c92ac1b64adc3b0c2db2139e1069abf1c672420
Assari Yumyuang <a-s-rock12@hotmail.com>
2024-03-02 20:06:24 +0700

    Template

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowsMs:10*60*1000,
+    max: 100
+});
+const hpp = require('hpp');
+const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const app = express();
+
+
+
+dotenv.config({path:'./config/config.env'});
+connectDB();
+
+
+
+const hospitals = require('./routes/restaurants');
+const auth = require('./routes/auth');
+const appointments = require('./routes/reservations');
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
+
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/appointments',appointments);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandleRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+
+    server.close(()=>process.exit(1));
+});
